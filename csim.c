e787fe5ee61aed20bfb81cfececce1d182432842
azazgit <anoshzahir@hotmail.com>
2021-05-16 15:36:52 +0100

    added base code for LRU.

--- csim.c
+++ csim.c
@@ -24,11 +24,157 @@ static int powfunc(int base, int exp) {
 
 /* Create the line struct */
 typedef struct{
-	unsigned long tag; 	// Store tag bits
-	bool valid;	// Store valid bit
-		
-	int lru;	// Store Least Recently Used status
+	unsigned long tag; 	// Store tag bits.
+	bool valid;		// Store valid bit
+	unsigned long pageNum	// Store the starting address of the block in cache.
 }line;
+
+/* LRU implementation */
+
+typedef struct QNode {
+	struct QNode * prev;
+	struct QNode * next;
+	unsigned long pageNumber;
+} QNode;
+
+/* A Queue*/
+typedef struct Queue {
+	unsigned count;
+	unsigned numberOfFrames;
+	QNode * front;
+	QNode * rear;
+} Queue;
+
+/* A hash (Collection of pointers to Queue Nodes). */
+typedef struct Hash {
+	int capacity;
+	QNode ** array;
+}
+
+/* A utility function to create a new Queue Node. The queue Node will store
+ * the given 'pageNumber' */
+QNode * newQNode(unsigned pageNumber) {
+	QNode * temp = (QNode *) malloc(sizeof(QNode));
+	temp->pageNumber = pageNumber;
+	temp->prev = temp->next = NULL;
+	return temp;
+}
+
+/* A utility function to create an empty Queue.
+ * The queue can have at most 'numberOfFrames' nodes. */
+Queue * createQueue(int numberOfFrames) {
+	Queue * queue = (Queue *) malloc(sizeof(Queue);
+	queue->count = 0;
+	queue->front = queue->rear = NULL;
+	queue->numberOfFrames = numberOfFrames;
+	return queue;
+}
+	
+/* A utility function to check if there is slot available in memory. */
+int AreAllFramesFull(Queue * queue) {
+	return queue->count == queue->numberOfFrames;
+}
+
+/* A utility function to check if queue is empty */
+
+int isQueueEmpty(Queue * queue) {
+	return queue->rear == NULL;
+}
+
+/* A utility function to delete a frame from queue */
+void deQueue(Queue * queue) {
+	if (isQueueEmpty(queue)) {
+		return;
+	}
+
+	// If this is the only node in the list, then change front.
+	if (queue->front == queue->rear) {
+		queue->front = NULL;
+	}
+
+	// Change rear and remove the previous rear.
+	QNode * temp = queue->rear;
+	queue->rear = queue->rear->prev;
+
+	if (queue->rear) {
+		queue->rear->next = NULL;
+	}
+
+	free(temp);
+
+	// Decrement the number of full frames by 1.
+	queue->count--;
+}
+
+/* A function to add a page with given 'pageNumber' to both queue and hash. */
+void Enqueue (Queue * queue, Hash * hash, unsigned pageNumber) {
+	
+	// If all frames are full, remove the page at the rear.
+	if (AreAllFramesFull(queue)) {
+		hash->array[queue->rear->pageNumber] = NULL;
+		deQueue(queue);
+	}
+
+	// Create a new node with given page number.
+	// And add the new node to the front of queue.
+	QNode * temp = newQNode(pageNumber);
+	temp->next = queue->front;
+
+	// If queue is empty, change both front and rear pointers.
+	if (isQueueEmpty(queue)) {
+	       queue->rear = queue->front = temp;
+	}
+
+	else { // Change the front.
+		queue->front->prev = temp;
+		queue->front = temp;
+	}
+
+	// Add page entry to hash also.
+	hash->array[pageNUmber] = temp;
+
+	// Increment number of full frames.
+	queue->count++;
+}
+
+/* This function is called when a page with given 'pageNumber' is referenced
+ * from cache (or memory). There are two cases:
+ * 1. Frame is not there in memory, we bring it in memory and add to the front
+ * of queue.
+ * 2. Frame is there is memory, we move the frame to front of queue.
+ */
+void ReferencePage(Queue * queue, Hash * hash, unsigned pageNumber) {
+	QNode * reqPage = hash->array[pageNumber];
+
+	// The page is not in cache, bring it.
+	if (reqPage == NULL) {
+		Enqueue(queue, hash, pageNumber);
+	}
+	else if (reqPage != queue->front) {
+		// Unlink requested page from its current location in queue.
+		reqPage->prev->next = reqPage->next;
+		if (reqPage->next) {
+			reqPage->next->prev = reqPage->prev;
+		}
+
+		// If the requested page is rear, then change rear as this
+		// node with be moved to front.
+		if (reqPage == queue->rear) {
+			queue->rear = reqPage->prev;
+			queue->rear->next = NULL;
+		}
+
+		// Put requested page before current front.
+		reqPage->next = queue->front;
+		reqPage->prev = NULL;
+
+		// Change prev of current front.
+		reqPage->next->prev = reqPage;
+
+		// Change front to the requested page.
+		queue->front = reqPage;
+	}
+}
 
 
 
