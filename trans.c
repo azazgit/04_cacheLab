af2946deffdee46a90f7eff3099f8e3e46139d39
azazgit <anoshzahir@hotmail.com>
2021-05-29 11:51:54 +0100

    renamed function for 32 by 32.

--- trans.c
+++ trans.c
@@ -68,8 +68,28 @@ void trans_00(int M, int N, int A[N][M], int B[M][N]) {
 }
 
 
-char trans_01_desc[] = "Transpose 01";
-void trans_01(int M, int N, int A[N][M], int B[M][N]) {
+char trans_32x32_8x8_id_desc[] = "32x32 8x8. Inline diag. Misses 343";
+void trans_32x32_8x8_id(int M, int N, int A[N][M], int B[M][N]) {
+    
+    int ii, jj, i, j;
+
+    // Traverse 8x8 tiles horizontally.
+    for(ii = 0; ii < N; ii += 8) {
+        for(jj = 0; jj < M; jj += 8){
+            
+            // For i != j, transfer all Aij to Bji.
+            for(i = ii; i < ii + 8; i++) {
+                for(j = jj; j < jj + 8; j++){
+                    B[j][i] = A[i][j];
+                }
+            }
+          // Move onto next tile. 
+        }
+    }
+}
+
+char trans_32x32_8x8_dd_desc[] = "32x32 8x8. Delay diag. Misses 289";
+void trans_32x32_8x8_dd(int M, int N, int A[N][M], int B[M][N]) {
     
     int ii, jj, i, j;
 
@@ -99,36 +119,6 @@ void trans_01(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
-char trans_16_desc[] = "Transpose 16 at a time.";
-void trans_16(int M, int N, int A[N][M], int B[M][N]) {
-    
-    int ii, jj, i, j;
-
-    int diag[16];
-
-    // Traverse 8x8 tiles horizontally.
-    for(ii = 0; ii < N; ii += 16) {
-        for(jj = 0; jj < M; jj += 16){
-            
-            // For i != j, transfer all Aij to Bji.
-            for(i = ii; i < ii + 16; i++) {
-                for(j = jj; j < jj + 16; j++){
-                    if (i == j) {
-                        diag[i%16] = A[i][i];
-                    }
-                    else {B[j][i] = A[i][j];}
-                }
-            }
-           // For i == j, transfer all Aii to Bii.
-           if (ii == jj) {
-              for (i = ii; i < ii + 16; i++) {
-                 B[i][i] = diag[i%16];
-              }
-           }
-          // Move onto next tile. 
-        }
-    }
-}
 
 char trans_64x64_4x4_desc[] = "64x64. 4x4. Delay diag. ";
 void trans_64x64_4x4(int M, int N, int A[N][M], int B[M][N]) {
@@ -161,6 +151,65 @@ void trans_64x64_4x4(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
+
+/* 
+ * trans - A simple baseline transpose function, not optimized for the cache.
+ */
+char trans_desc[] = "Simple row-wise scan transpose";
+void trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }    
+
+}
+
+
+
+/*
+ * registerFunctions - This function registers your transpose
+ *     functions with the driver.  At runtime, the driver will
+ *     evaluate each of the registered functions and summarize their
+ *     performance. This is a handy way to experiment with different
+ *     transpose strategies.
+ */
+void registerFunctions()
+{
+    /* Register your solution function */
+    //registerTransFunction(transpose_submit, transpose_submit_desc); 
+
+    /* Register any additional transpose functions */
+    
+    //registerTransFunction(trans_00, trans_00_desc);
+    registerTransFunction(trans_32x32_8x8_id, trans_32x32_8x8_id_desc);
+    registerTransFunction(trans_32x32_8x8_dd, trans_32x32_8x8_dd_desc);
+    //registerTransFunction(trans_64x64_4x4, trans_64x64_4x4_desc);
+    
+}
+
+/* 
+ * is_transpose - This helper function checks if B is the transpose of
+ *     A. You can check the correctness of your transpose by calling
+ *     it before returning from the transpose function.
+ */
+int is_transpose(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; ++j) {
+            if (A[i][j] != B[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
 char trans_algo_desc[] = "Youtube algo";
 void trans_algo(int M, int N, int A[N][M], int B[M][N])
@@ -296,65 +345,3 @@ void trans_algo64(int M, int N, int A[N][M], int B[M][N]) {
        }
     }
 }
-/* 
- * trans - A simple baseline transpose function, not optimized for the cache.
- */
-char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
-}
-
-
-
-/*
- * registerFunctions - This function registers your transpose
- *     functions with the driver.  At runtime, the driver will
- *     evaluate each of the registered functions and summarize their
- *     performance. This is a handy way to experiment with different
- *     transpose strategies.
- */
-void registerFunctions()
-{
-    /* Register your solution function */
-    //registerTransFunction(transpose_submit, transpose_submit_desc); 
-
-    /* Register any additional transpose functions */
-    //registerTransFunction(trans, trans_desc); 
-    
-    //registerTransFunction(trans_00, trans_00_desc);
-    registerTransFunction(trans_01, trans_01_desc);
-    registerTransFunction(trans_16, trans_16_desc);
-    //registerTransFunction(trans_64x64_4x4, trans_64x64_4x4_desc);
-    //registerTransFunction(trans_algo2, trans_algo2_desc);
-    //registerTransFunction(trans_algo64, trans_algo64_desc);
-    
-}
-
-/* 
- * is_transpose - This helper function checks if B is the transpose of
- *     A. You can check the correctness of your transpose by calling
- *     it before returning from the transpose function.
- */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
