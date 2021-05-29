b0fef904bf8110af7bdf65683966082b48c97d89
azazgit <anoshzahir@hotmail.com>
2021-05-29 15:55:32 +0100

    tidied up code in submit func. Comments added.

--- trans.c
+++ trans.c
@@ -9,7 +9,7 @@
  */ 
 #include <stdio.h>
 #include "cachelab.h"
-#define tileLength 8
+#define tileLength 8 // Used to find the best miss rate. Not used in submit func.
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 int min(int n1, int n2){return (n1 > n2) ? n2 : n1;}
@@ -23,20 +23,29 @@ int min(int n1, int n2){return (n1 > n2) ? n2 : n1;}
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
-    
+
+/* Assignment says can't use arrays. So can swap for 8 local vars if necessary.
+ * 
+ * Code be tidied up for M = 32 and 64. There is alot of duplicate code, getting 
+ * rid of which will require the use of a local var to hold tileLength and
+ * assign to it 8 for 32x32 and 4 for 64x64 tiles.
+ * 
+ * The code for 61x67 does not delay transposing the diagonals. This is because
+ * the tileLength of 17 gives the best miss rate. Saving the 17 diag elems of
+ * the tile in local vars[or array] will exceed the max of 12 allowed local vars. 
+ */ 
     int ii, jj, i, j;
     
     // 32 x 32 matrix. Blocksize 8 gives the best miss rate.
     if (M == 32) {
-        int diag[8]; // Store diag values of the tile.
-
+        int diag[8]; // For diag vals of tile.
         // Traverse 8x8 tiles horizontally.
         for(ii = 0; ii < N; ii += 8) {
             for(jj = 0; jj < M; jj += 8){
             
                 // For i != j, transfer all Aij to Bji.
-                for(i = ii; i < min(N, ii + 8); i++) {
-                    for(j = jj; j < min(M, jj + 8); j++){
+                for(i = ii; i < ii + 8; i++) {
+                    for(j = jj; j < jj + 8; j++){
                         if (i == j) {
                             diag[i%8] = A[i][i];
                         }
@@ -63,8 +72,8 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
             for(jj = 0; jj < M; jj += 4){
             
             // For i != j, transfer all Aij to Bji.
-            for(i = ii; i < min(N, ii + 4); i++) {
-                for(j = jj; j < min(M, jj + 4); j++){
+            for(i = ii; i < ii + 4; i++) {
+                for(j = jj; j < jj + 4; j++){
                     if (i == j) {
                         diag[i%4] = A[i][i];
                     }
@@ -105,6 +114,7 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
  * a simple one below to help you get started. 
  * 
 
+
 char trans_00_desc[] = "Transpose 00";
 void trans_00(int M, int N, int A[N][M], int B[M][N]) {
     
@@ -141,6 +151,7 @@ void trans_3232_id(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
+
 char trans_3232_dd_desc[] = "3232. Delay diag. 289 misses with tilelength 8.";
 void trans_3232_dd(int M, int N, int A[N][M], int B[M][N]) {
     
@@ -173,7 +184,6 @@ void trans_3232_dd(int M, int N, int A[N][M], int B[M][N]) {
 }
 
 
-
 char trans_6464_id_desc[] = "6464. Inline diag. Misses 1891.";
 void trans_6464_id(int M, int N, int A[N][M], int B[M][N]) {
     
@@ -193,6 +203,8 @@ void trans_6464_id(int M, int N, int A[N][M], int B[M][N]) {
         }
     }
 }
+
+
 char trans_6464_dd_desc[] = "6464. Delay diag. Misses 1797.";
 void trans_6464_dd(int M, int N, int A[N][M], int B[M][N]) {
     
@@ -245,6 +257,7 @@ void trans_6167_id(int M, int N, int A[N][M], int B[M][N]) {
     }
 }
 
+
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
@@ -261,7 +274,6 @@ void trans(int M, int N, int A[N][M], int B[M][N])
     }    
 
 }
-
 
 
 /*
@@ -283,6 +295,7 @@ void registerFunctions()
     
 }
 
+
 /* 
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
@@ -302,6 +315,10 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     return 1;
 }
 
+
+/* The below functions tried to implement with success the following algo:
+ * https://www.youtube.com/watch?v=v9Y2uiThKBY&t=563s
+ */
 char trans_algo_desc[] = "Youtube algo";
 void trans_algo(int M, int N, int A[N][M], int B[M][N])
 {
